132e306065ef51150880720b04b68fce6edd209a
vagrant <vagrant@precise32.(none)>
2014-02-19 23:48:04 +0000

    Database Lab

--- routes/project.js
+++ routes/project.js
@@ -2,7 +2,10 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
-
+  models.Project
+    .find({"_id": projectID})
+    //.sort('-date')
+    .exec(afterQuery);
   // query for the specific project and
   // call the following callback
 
@@ -15,14 +18,34 @@ exports.projectInfo = function(req, res) { 
 exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
+  var newPost = new models.Project({
+    "title": form_data['project_title'],
+    "date": Date.parse(form_data['date']),
+    "summary": form_data['summary'],
+    "image": form_data['image_url']
+  })
+  newPost.save(afterSaving);
 
+  function afterSaving(err) {
+    if(err) {console.log(err); res.send(500);}
+    res.send(200);
+  }
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
+  //console.log(projectID);
+  models.Project
+    .find({"_id": projectID })
+    .remove()
+    .exec(afterRemoving);
 
+    function afterRemoving(err, deletedBlogs) {
+      if(err) {console.log(err); res.send(500);}
+      res.send(200);
+    }
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
 }
